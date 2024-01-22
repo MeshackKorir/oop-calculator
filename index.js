ddf2beb2dd3bfc5a6048ddc66be3996dffcb5b8a
MeshackKorir <meshackkorir100@gmail.com>
2024-01-22 19:40:02 +0300

    html and css and javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+class Calculator {
+    constructor(){
+        this.currentOperand = document.getElementById('current-operand');
+       this.previousOperand = document.getElementById('previous-operand');
+       this.clear(); 
+    }
+}
