45ac3cfc1b411e239617a7add18996b1ee4be810
MeshackKorir <meshackkorir100@gmail.com>
2024-01-22 19:49:20 +0300

    sum,subtract,divide and multiply in class

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-class Calculator {
+class Calculator{
     constructor(){
-        this.currentOperand = document.getElementById('current-operand');
-       this.previousOperand = document.getElementById('previous-operand');
-       this.clear(); 
+        this.currentResult = 0;
+    }
+
+    add(number) {
+        this.currentResult += number;
+    }
+
+    subtract(number) {
+        this.currentResult -= number;
+    }
+
+    multiply(number) {
+        this.currentResult *= number;
+    }
+
+    divide(number) {
+        if (number === 0) {
+            console.error("number divide by zero");
+            return;
+        }
+        this.currentResult /= number;
+    }
+
+    getResult() {
+        return this.currentResult;
+    }
+
+    clear() {
+        this.currentResult = 0;
     }
 }
+
+
